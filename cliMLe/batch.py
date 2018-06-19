5e6b699d815761d6b8df2bafd14deb6b13abd6c6
Thomas Maxwell <thomas.maxwell@nasa.gov>
2018-06-19 18:16:58 -0400

    Tests.

--- cliMLe/batch.py
+++ cliMLe/batch.py
@@ -34,8 +34,9 @@ class BatchServer:
         if (self.shuffleMode == EPOCH) and (self.batchIndex == 0):
             self.perm_series = np.random.permutation(self.series)
         pc_batch = self.perm_series[start_index:end_index]
-        perm_batch =  np.random.permutation(pc_batch).transpose() if (self.shuffleMode == BATCH) else pc_batch.transpose()
-        self.currentBatch = perm_batch[0:self.input_size].transpose(), perm_batch[self.input_size:].transpose()
+        perm_batch =  np.random.permutation(pc_batch) if (self.shuffleMode == BATCH) else pc_batch
+        perm_batch_trans = perm_batch.transpose()
+        self.currentBatch = perm_batch_trans[0:self.input_size].transpose(), perm_batch_trans[self.input_size:].transpose()
         return self.currentBatch
 
     def getCurrentBatch(self):
@@ -47,23 +48,38 @@ if __name__ == "__main__":
     data_path = 'https://dataserver.nccs.nasa.gov/thredds/dodsC/bypass/CREATE-IP/Reanalysis/NASA-GMAO/GEOS-5/MERRA2/mon/atmos/' + varName + '.ncml'
     outDir = "/tmp/"
     start_year = 1980
-    end_year = 2015
-    nModes = 32
+    end_year = 1982
+    nModes = 3
     experiment = projectName + '_' + str(start_year) + '-' + str(end_year) + '_M' + str(nModes) + "_" + varName
     project = Project(outDir,projectName)
 
-    td = ProjectDataSource( "HadISST_1.cvdp_data.1980-2017", [ "indian_ocean_dipole", "pdo_timeseries_mon", "amo_timeseries_mon" ], ( "1980-1-1", "2014-12-31" ) )
+    td = ProjectDataSource( "HadISST_1.cvdp_data.1980-2017", [ "indian_ocean_dipole", "pdo_timeseries_mon", "amo_timeseries_mon" ], ( "1980-1-1", "1981-12-31" ) )
     dset = TrainingDataset( [td] )
 
     print
     print "SHUFFLE: BATCH"
-    bserv = BatchServer( project, experiment, dset, 80, BATCH )
+    bserv = BatchServer( project, experiment, dset, 3, BATCH )
     print "EPOC Size: " + str(  bserv.series.shape )
     print "NBatches: " + str(  bserv.numBatches )
-    for iBatch in range(3):
+
+    print "\nEPOC 1"
+    for iBatch in range( bserv.numBatches ):
         input, output = bserv.getNextBatch()
         print "Batch " + str( iBatch ) + ", input shape = " + str( input.shape ) + ", output shape = " + str( output.shape )
+        print "Input:"
+        print str( input )
+        print "Output:"
+        print str( output )
 
+
+    print "\nEPOC 2"
+    for iBatch in range( bserv.numBatches ):
+        input, output = bserv.getNextBatch()
+        print "Batch " + str( iBatch ) + ", input shape = " + str( input.shape ) + ", output shape = " + str( output.shape )
+        print "Input:"
+        print str( input )
+        print "Output:"
+        print str( output )
 
 
 
